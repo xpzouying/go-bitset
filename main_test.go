866cf59e10c7416c86e4d2acdfbdac38f7e5f146
Ying Zou <xpzouying@gmail.com>
2017-08-25 15:35:26 +0800

    Add support for go1.9
    * travis config
    * testing enhance

--- main_test.go
+++ main_test.go
@@ -1,34 +1,74 @@
 package bitset
 
 import (
+	"runtime"
+	"strconv"
+	"strings"
 	"testing"
 )
 
+// is suppport testing.Helper()
+// true: support; false: not support
+func enhanceTestingHelper(t *testing.T) bool {
+	v := runtime.Version() // like go1.9
+	version, err := strconv.ParseFloat(strings.TrimPrefix(v, "go"), 64)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if version >= 1.9 {
+		return true
+	}
+
+	return false
+
+}
+
 func assertNil(t *testing.T, v interface{}) {
+	if enhanceTestingHelper(t) {
+		t.Helper()
+	}
+
 	if v != nil {
 		t.Errorf("should be nil, but not: %v", v)
 	}
 }
 
 func assertNotNil(t *testing.T, v interface{}) {
+	if enhanceTestingHelper(t) {
+		t.Helper()
+	}
+
 	if v == nil {
 		t.Error("should not be nil, but is nil")
 	}
 }
 
 func assertTrue(t *testing.T, v bool) {
+	if enhanceTestingHelper(t) {
+		t.Helper()
+	}
+
 	if v != true {
 		t.Errorf("should be true, but not: %v", v)
 	}
 }
 
 func assertFalse(t *testing.T, v bool) {
+	if enhanceTestingHelper(t) {
+		t.Helper()
+	}
+
 	if v != false {
 		t.Errorf("should be false, but not: %v", v)
 	}
 }
 
 func assertError(t *testing.T, err, expErr error) {
+	if enhanceTestingHelper(t) {
+		t.Helper()
+	}
+
 	if err != expErr {
 		t.Errorf("error should be %v, but be %v", expErr, err)
 	}
